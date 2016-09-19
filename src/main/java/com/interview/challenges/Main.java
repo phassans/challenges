4cc0ca07326eb252dfc4563283a08819410bd185
Pramod Hassan Shashidhara <phassanshashidha@paypal.com>
2016-09-18 23:43:13 -0700

    Array Programs

--- src/main/java/com/interview/challenges/Main.java
+++ src/main/java/com/interview/challenges/Main.java
@@ -1,11 +1,23 @@
 package com.interview.challenges;
 
+import com.interview.challenges.arrays.ArraysEasy;
 import com.interview.challenges.basic.Basic;
 
 public class Main {
     public static void main(String[] args) {
         Basic basic = new Basic();
-        evaluateBasics(basic);
+        // evaluateBasics(basic);
+
+        ArraysEasy arrayEasy = new ArraysEasy();
+        evaluateArrayEasy(arrayEasy);
+    }
+
+    private static void evaluateArrayEasy(ArraysEasy arrayEasy) {
+        int[] arr = { 13, 18, 13, 14, 13, 16, 14, 21, 13 };
+        System.out.println("Mean " + arrayEasy.findMean(arr));
+        System.out.println("Median " + arrayEasy.findMedian(arr));
+        System.out.println("Mode " + arrayEasy.findMode(arr));
+        System.out.println("Range " + arrayEasy.findRange(arr));
     }
 
     public static void evaluateBasics(Basic basic) {
@@ -22,9 +34,9 @@ public class Main {
         for (int i = 1; i <= 8; i++) {
             System.out.print(basic.fibonacciRecursive(i) + ", ");
         }
-        
+
         basic.generatePrimesBetween(100);
-        
+
         basic.generateNPrimes(100);
     }
 }
--- src/main/java/com/interview/challenges/arrays/ArraysEasy.java
+++ src/main/java/com/interview/challenges/arrays/ArraysEasy.java
@@ -1,4 +1,47 @@
 package com.interview.challenges.arrays;
 
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
 public class ArraysEasy {
+    public int findMean(int[] arr) {
+        int sum = 0;
+        for (int i = 0; i < arr.length; i++) {
+            sum += arr[i];
+        }
+        return sum / arr.length;
+    }
+
+    public int findMedian(int[] arr) {
+        Arrays.sort(arr);
+        return arr[arr.length / 2];
+    }
+
+    public int findMode(int[] arr) {
+        HashMap<Integer, Integer> map = new HashMap<Integer, Integer>();
+        for (int i = 0; i < arr.length; i++) {
+            if (map.containsKey(arr[i])) {
+                map.put(arr[i], map.get(arr[i]) + 1);
+            } else {
+                map.put(arr[i], 1);
+            }
+        }
+        int max = 1;
+        int maxKey = 1;
+        for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
+            Integer key = entry.getKey();
+            Integer value = entry.getValue();
+            if (value > max) {
+                max = value;
+                maxKey = key;
+            }
+        }
+        return maxKey;
+    }
+
+    public int findRange(int[] arr) {
+        Arrays.sort(arr);
+        return arr[arr.length - 1] - arr[0];
+    }
 }
